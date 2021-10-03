267a1d6cf5b81cb166008126cbd69f67a764e1ec
Nishant <nishantbhosale244@gmail.com>
2021-10-03 09:55:47 +0530

    Finished setup

--- config/db.js
+++ config/db.js
@@ -13,4 +13,4 @@ const connectDB = async () => {
 	}
 };
 
-export default connectDB;
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const PORT = 5000 || process.env.PORT;
+
+dotenv.config();
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.json({ message: "Working" });
+});
 
 app.listen(PORT, () => {
 	console.log("Server running on " + PORT);
