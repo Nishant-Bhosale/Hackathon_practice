502054efab77c2e71ae6adf79e5160d489d0a96e
Nishant <nishantbhosale244@gmail.com>
2021-10-04 12:50:38 +0530

    Added story and comment models

---
+++ backend/models/Comment.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const CommentSchema = new Schema(
+	{
+		text: {
+			type: String,
+		},
+		authorName: {
+			type: String,
+			required: true,
+		},
+		commentedBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+const Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
